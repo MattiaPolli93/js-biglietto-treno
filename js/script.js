e93e59d9133bb9d869432f15d630e273a77db9ad
Mattia <matpolli93@gmail.com>
2021-02-26 17:00:55 +0100

    Improvement

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 // Train ticket price
-// Km
+
+// Asking for Km
 var km = prompt("Hello! How far would you like to travel? (Specify the amount of Km please)");
 
-// Age
-var age = prompt("How old are you?");
-
-// Total price: 0.21€ per Km
-var price = km * 0.21;
-
-if (age < 18) {
-    // 20% discount
-    price = price - (price * 0.2);
-} else if (age >= 65) {
-    // 40% discount
-    price = price - (price * 0.4);
+// Checking whether the inserted number for Km is an actual number
+if (isNaN(km)) {
+    // Checking one time whether the input for km is an actual number
+    km = prompt("Sorry, but we need an actual number");
+    //Checking another time
+} if (isNaN(km)) {
+    km = alert("Mmm, we need an actual number (please do it again by reloading the page...)");
+    //Checking that the amount of Km is not equal or less than 0
+} else if (km <= 0) {
+    alert("You think this is funny?");
+} else {
+    // Asking for Age
+    var age = prompt("How old are you?");
+    // Checking that age is not 0 or less
+    if (isNaN(age)) {
+        age = prompt("This is not a valid number for your age I'm afraid... try again!");
+    }
+    else {
+        // Price
+        var price = km * 0.21;        
+        // Calculating price
+        if (age < 18) {
+            // 20% discount
+            price = price - (price * 0.2);
+        } else if (age >= 65) {
+            // 40% discount
+            price = price - (price * 0.4);
+        }
+        // Displayed message
+        document.getElementById("price").innerHTML = "Perfect! This is the amount to pay for your train ticket: " + price + "€";
+    }
 }
-
-// Displayed message
-document.getElementById("price").innerHTML = "Perfect! This is the amount to pay for your train ticket: " + price + "€";
